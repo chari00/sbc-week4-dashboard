1b39703711b0f98f28022f57ff63a30ec9bb0c3e
chari00 <macharitocruzuk@gmail.com>
2024-10-11 17:09:35 +0100

    create modal as form to fill in the user's task to add.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,19 @@
-console.log('hello')
-// items draggable
-$( function() {
-    $( "#sortable" ).sortable({
-      revert: true
-    });
-    $( "#draggable" ).draggable({
-      connectToSortable: "#sortable",
-      helper: "clone",
-      revert: "invalid"
-    });
-    $( "ul, li" ).disableSelection();
-  } );
+
+
+//this initialize the flatpickr
+document.addEventListener("DOMContentLoaded", function () {
+  flatpickr("#datetime", {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+  });
+});
+
+//this sets the date and time picker format
+flatpickr("#datetime", {
+  enableTime: true,
+  dateFormat: "Y-m-d H:i",
+  altInput: true,
+  altFormat: "F j, Y at h:i K",
+  minDate: "today",
+  maxDate: new Date().fp_incr(),
+});
