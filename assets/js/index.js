f8f89e32df5026d906dde7e7a69ddefd32ac1420
chari00 <macharitocruzuk@gmail.com>
2024-10-13 12:48:51 +0100

    add event listener to modal.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,9 @@
-const searchBtn = document.getElementById('searchButton');
-
+const searchBtn = document.getElementById("searchButton");
+let taskTitleModal = document.getElementById("task-title");
+let datetimeModal = document.getElementById("datetime");
+let dropdownModal = document.getElementById("event-dropdown");
+let descriptionModal = document.getElementById("description");
+const saveBtn = document.getElementById("save");
 
 //this initialize the flatpickr
 document.addEventListener("DOMContentLoaded", function () {
@@ -18,14 +22,28 @@ flatpickr("#datetime", {
   minDate: "today",
   maxDate: new Date().fp_incr(),
 });
-
-searchBtn.addEventListener('click', search);
-function search(e) {
-  const userInput = document.getElementById('searchTerm').value;
-  const search = document.getElementById('searches')
-  const li =document.createElement('li')
-  li.innerHTML = userInput
-  search.append(li)
-  document.getElementById('searchTerm').value = "";
+//this triggers the event for search button
+searchBtn.addEventListener("click", search);
+function search() {
+  const userInput = document.getElementById("searchTerm");
+  const search = document.getElementById("searches");
+  //checks if the user input is empty
+  if (userInput.value === "") {
+    return alert("Please enter your search");
+  }
+  const li = document.createElement("li");
+  li.innerHTML = userInput.value;
+  search.append(li);
+  userInput.value = "";
 }
 
+const saveTask = (e) => {
+  e.preventDefault();
+  console.log("save button is clicked");
+  // taskTitle.textContent = e.target.value;
+  // datetimeModal = e.target.value;
+  // dropdownModal = e.target.value;
+  // descriptionModal.textContent = e.target.value;
+};
+//this triggers the add new button
+saveBtn.addEventListener("click", saveTask);
